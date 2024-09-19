0c6ce038931b1d7e863506a040c16ef9618c46db
benebene84 <benedikt.sperl@gmail.com>
2024-09-19 21:48:50 +0200

    update og graph for homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export const metadata: Metadata = {
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Benedikt Sperl',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Benedikt Sperl',
     locale: 'de_DE',
     type: 'website',
   },

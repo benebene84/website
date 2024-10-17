d946ccb79bebde75dc61702e65499787277e6a81
benebene84 <benedikt.sperl@gmail.com>
2024-10-17 21:22:10 +0200

    update tailwind alpha and switch

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { Switch } from './ui/switch'
 
 const navItems = {
   '/': {
--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { cx } from 'app/utils/cx'
 import { InputHTMLAttributes } from 'react'
 
-type SwitchProps = InputHTMLAttributes<HTMLInputElement>
+type SwitchProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
+  onChange?: (checked: boolean) => void
+}
 
-export const Switch = ({ className, ...props }: SwitchProps) => {
+export const Switch = ({ className, onChange, ...props }: SwitchProps) => {
   return (
     <input
       type="checkbox"
@@ -11,6 +14,9 @@ export const Switch = ({ className, ...props }: SwitchProps) => {
         'focus-visible:ring-light-blue-300 relative flex h-7 w-12 cursor-pointer appearance-none items-center rounded-full bg-gray-500 transition-colors duration-500 ease-out before:absolute before:left-1 before:h-5 before:w-5 before:rounded-full before:bg-white before:transition-transform before:duration-500 before:ease-out before:content-[""] checked:bg-green-500 checked:before:translate-x-[1.125rem] focus:outline-none focus-visible:ring-2',
         className,
       )}
+      onChange={(e) => {
+        onChange?.(e.target.checked)
+      }}
       {...props}
     />
   )

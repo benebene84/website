0893ebd9a648e86522510c45f71ce94065bf1aa5
benebene84 <benedikt.sperl@gmail.com>
2025-05-18 14:25:50 +0200

    new blog post delivery vs. management / consulting

--- app/components/ui/skip-link.tsx
+++ app/components/ui/skip-link.tsx
@@ -1,15 +1,15 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes } from 'react'
 
-type SkipLinkProps = HTMLAttributes<HTMLAnchorElement>;
+type SkipLinkProps = HTMLAttributes<HTMLAnchorElement>
 
 export const SkipLink = ({ ...props }: SkipLinkProps) => {
   return (
     <a
       href="#main"
-      className="absolute top-[-9999px] left-[-9999px] z-50 -translate-y-full transform px-4 py-2 transition focus:translate-y-0 focus:top-0 focus:left-0"
+      className="absolute top-[-9999px] left-[-9999px] z-50 -translate-y-full transform px-4 py-2 transition focus:top-0 focus:left-0 focus:translate-y-0"
       {...props}
     >
       Skip to main content
     </a>
-  );
-};
+  )
+}

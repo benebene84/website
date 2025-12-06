5cc697a8fc48df85866828a1b4bfc5868852dcfd
benebene84 <benedikt.sperl@gmail.com>
2025-12-06 20:17:51 +0100

    roll back changes for og image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,9 +5,9 @@ export function GET(request: Request) {
   const title = url.searchParams.get('title')
 
   return new ImageResponse(
-    <div className="flex flex-col w-full h-full items-center justify-center bg-white">
-      <div className="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-        <h2 className="flex flex-col text-4xl font-bold tracking-tight text-left">
+    <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+      <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+        <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
           {title}
         </h2>
       </div>

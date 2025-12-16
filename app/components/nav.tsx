9bff484c7df683f38fc7cec9354c6e0793706129
benebene84 <benedikt.sperl@gmail.com>
2025-12-16 20:35:09 +0100

    add loading, clean-up nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -15,26 +15,24 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="mb-12 tracking-tight md:mb-8">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="fade relative flex scroll-pr-6 flex-row items-start px-0 pb-0 md:relative md:overflow-auto"
-          id="nav"
-        >
-          <div className="flex flex-row items-center gap-6 space-x-0 sm:gap-8">
-            {Object.entries(navItems).map(([path, { name }], index) => {
-              return (
-                <Link
-                  key={path + index}
-                  href={path}
-                  className="relative flex py-1 align-middle text-lg transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
-      </div>
+      <nav
+        className="fade relative flex scroll-pr-6 flex-row items-start px-0 pb-0 md:relative md:overflow-auto"
+        id="nav"
+      >
+        <div className="flex flex-row items-center gap-6 space-x-0 sm:gap-8">
+          {Object.entries(navItems).map(([path, { name }], index) => {
+            return (
+              <Link
+                key={path + index}
+                href={path}
+                className="relative flex py-1 align-middle text-lg transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
+              >
+                {name}
+              </Link>
+            )
+          })}
+        </div>
+      </nav>
     </aside>
   )
 }

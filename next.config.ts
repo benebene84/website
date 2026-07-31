3483af631d234c4a9c8e3f94ee14f13cc586676b
benebene84 <benedikt.sperl@gmail.com>
2026-07-31 13:46:13 +0200

    update dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  experimental: {
+    useTypeScriptCli: true,
+  },
 }
 
 export default withContentCollections(nextConfig)
